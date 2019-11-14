ddcdaf03a0238edc65a8437dc5d22378cba76816
FredaFei <964959033@qq.com>
2019-11-14 10:45:22 +0800

    login

--- src/views/loginSignup/login.tsx
+++ src/views/loginSignup/login.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
-import classes, {createScopedClasses} from '@/utils/classnames'
+import classes, { createScopedClasses } from '@/utils/classnames'
 import utils from '@/utils/help'
 import * as Cookies from 'js-cookie';
 import './index.scss'
-import {RouteComponentProps} from "react-router";
+import { RouteComponentProps } from "react-router";
+import { Button } from "components/index";
 
 const componentName = 'Login'
 const sc = createScopedClasses(componentName)
@@ -15,14 +16,12 @@ interface Props extends RouteComponentProps {
 
 const Login: React.FunctionComponent<Props> = props => {
   const onClick = () => {
-    // Cookies.set('auth', 'test')
-    Cookies.set('auth','test')
-    window.localStorage.setItem('auth', 'test')
+    Cookies.set('auth', 'test')
     props.history.replace(utils.getQueryString('fromUrl') || '')
   }
   return (
     <div className={classes(sc('wrapper'))}>
-      <button onClick={onClick}>login</button>
+      <Button onClick={onClick}>login</Button>
     </div>
   );
 }
